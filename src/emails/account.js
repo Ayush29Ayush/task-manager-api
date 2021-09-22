eacac12722f8ae07541ea70244662e4768d17624
Ayush <ayushsenapati123@gmail.com>
2021-09-22 13:37:56 +0530

    added mocks for test cases

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,23 +1,8 @@
-const sgMail = require('@sendgrid/mail')
 
-// const sendgridAPIKey = ''
+const sgMail = require('@sendgrid/mail')
 
 // sgMail.setApiKey(sendgridAPIKey)
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
-//! Goal: Pull JWT secter and database URL into env vars
-//1. Create two new env vars: JWT_SECRET and MONGODB_URL
-//2. Setup values for each in the developement env files
-//3. Swap out three hrdcoded values
-//4. Test your work. Create new user and get their profile.
-
-// sgMail.send({
-//     // to: 'ayushsenapati123@gmail.com',
-//     to: 'ayush.senapati2019@vitstudent.ac.in',
-//     from: 'ayushsenapati123@gmail.com',
-//     subject: 'This is a trial mail from my task app',
-//     text: 'I hope this one actually gets to you.'
-// })
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
@@ -48,12 +33,6 @@ const sendCancelationEmail = (email, name) => {
     })
 }
 
-//! Goal: Send email to user on cancellation
-//1. Setup a new function for sending an email on cancellation
-//   - email and name as args
-//2. Include their name in the email and ask why they cancelled
-//3. call it just after the account is removed
-//4. Run the request and check your inbox
 
 module.exports = {
     sendWelcomeEmail,
