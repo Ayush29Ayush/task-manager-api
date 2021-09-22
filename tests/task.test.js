c734099de1fb84cc8c9e9d060588bb1336868f97
Ayush <ayushsenapati123@gmail.com>
2021-09-22 18:58:32 +0530

    Added new task test cases

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,7 +1,14 @@
 const request = require('supertest')
 const app = require('../src/app')
 const Task = require('../src/models/task')
-const { userOneId, userOne , setupDatabase } = require('./fixtures/db')
+const { userOneId,
+        userOne ,
+        userTwoId,
+        userTwo,
+        taskOne,
+        taskTwo,
+        taskThree,
+        setupDatabase } = require('./fixtures/db')
 
 beforeEach(setupDatabase)
 
@@ -17,4 +24,38 @@ test('Should create task for user',async()=>{
     const task = await Task.findById(response.body._id)
     expect(task).not.toBeNull()
     expect(task.completed).toEqual(false)
+})
+
+
+//! Goal: Test GET /tasks
+//1. Request all tasks for user one
+//2. Assert the correct status code
+//3. Check the length of the response array is 2
+//4. Test your work!
+
+test('Should fetch user tasks', async()=>{
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization',`Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+    expect(response.body.length).toEqual(2)
+})
+
+//! Goal: Test delete task security
+//1. Attempt to have the second user delete the first task (should fail)
+//   - Setup necessary exports from db.js
+//2. Assert the failed status code
+//3. Assert the task is still in the database
+//4. Test your work
+
+test('Should not delete other users tasks', async()=>{
+    const response = await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization',`Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404)
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
+
 })
